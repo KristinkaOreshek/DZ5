96f0451a1d4ae4fb5937f975a7271ee020d1bec4
Kristina <tinaore@mail.ru>
2023-12-01 11:22:58 +0300

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate (int ves,double rost){
+        return (int) (ves/rost/rost);
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int calculate = service.calculate(50, 1.70);
+        System.out.println(calculate);
+    }
+}
