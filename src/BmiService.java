43f17d292a5abd77dfe0784b86a88538cb55e0c8
Kristina <tinaore@mail.ru>
2023-12-03 13:46:41 +0300

    first commit4

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate (int weight,double height){
-        return (int) (weight/height/height);
+    public int calculate(int weight, double height) {
+        return (int) (weight / height / height);
 
     }
 
